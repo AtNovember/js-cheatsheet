48744b6388ba8afac24a03d90d2b29aecc2a5be9
Inessa Pokromkina <atnovemb@gmail.com>
2019-06-15 18:14:29 +0300

    add link for source

--- data_structures/02-tree-oop.js
+++ data_structures/02-tree-oop.js
@@ -1,16 +1,4 @@
-// Node
-//  data — здесь хранятся значения;
-//  parent — указывает на родительский элемент узла;
-//  children — указывает на следующий узел в списке.
-//
-// Tree
-//  _root — указывает на корневой узел дерева;
-//  traverseDF(callback) — проходит узлы дерева с помощью метода DFS;
-//  traverseBF(callback) — проходит узлы дерева с помощью метода BFS;
-//  contains(data, traversal) — ищет узел дерева;
-//  add(data, toData, traverse) — добавляет узел к дереву;
-//  remove(child, parent) — удаляет узел дерева.
-
+// http://blog.benoitvallon.com/data-structures-in-javascript/the-tree-data-structure/
 
 class Node {
   constructor(data) {
