d5695b0dec08b00c89d3634f403e6dc2ab232641
Inessa Pokromkina <atnovemb@gmail.com>
2019-06-15 18:12:52 +0300

    tree structure

--- data_structures/02-tree-oop.js
+++ data_structures/02-tree-oop.js
@@ -15,49 +15,175 @@
 class Node {
   constructor(data) {
     this.data = data;
-    this.parent = null;
     this.children = [];
   }
 }
 
 class Tree {
-  constructor(data) {
-    const node = new Node(data);
-    this._root = node;
+  constructor() {
+    this.root = null;
   } 
 
-  contains(callback, traversal) {
-    traversal.call(this, callback);
-  }
-
-  add(data, toData, traversal) {
-    const child = new Node(data);
-    const parent = null;
-    const callback = (node) => {
-      if(node.data === toData) {
-        parent = node;
-      }
-    }
-
-    this.contains(callback, traversal);
+  add(data, toNodeData) {
+    const node = new Node(data);
+    const parent = toNodeData ? this.findBFS(toNodeData) : null;
 
     if (parent) {
-      parent.children.push(child);
-      child.parent = parent;
+      parent.children.push(node);
     } else {
-      throw new Error('Cannot add node to a non-existent parent.');
+      if(!this.root) {
+        this.root = node;
+      } else {
+        return 'Root node is already assigned';
+      }
     }
   }
+
+  remove(data) {
+    if (this.root.data === data) {
+      this.root = null;
+    }
+
+    const queue = [this.root];
+    while(queue.length){
+      const node = queue.shift();
+      for (let i = 0; i < node.children.length; i++) {
+        if (node.children[i].data === data) {
+          node.children.splice(i, 1);
+        } else {
+          queue.push(node.children[i]);
+        }
+      }
+    }
+  }
+
+  contains(data) {
+    return this.findBFS(data) ? true : false;
+  }
+
+  findBFS(data) {
+    const queue = [this.root];
+    while(queue.length) {
+      const node = queue.shift();
+      if (node.data === data) {
+        return node;
+      }
+
+      for(let i = 0; i < node.children.length; i++) {
+        queue.push(node.children[i]);
+      }
+    }
+    return null;
+  }
+
+  _preOrder(node, fn) {
+    if(node) {
+      if(fn) {
+        fn(node);
+      }
+    }
+    for(let i = 0; i < node.children.length; i++) {
+      this._preOrder(node.children[i], fn);
+    }
+  }
+
+  _postOrder(node, fn) {
+    if (node) {
+      for (var i = 0; i < node.children.length; i++) {
+        this._postOrder(node.children[i], fn);
+      }
+      if (fn) {
+        fn(node);
+      }
+    }
+  }
+
+  traverseDFS(fn, method) {
+    const current = this.root;
+    if (method) {
+      this['_' + method](current, fn);
+    } else {
+      this._preOrder(current, fn);
+    }
+  }
+
+  traverseBFS(fn) {
+    const queue = [this.root];
+    while (queue.length) {
+      const node = queue.shift();
+      if (fn) {
+        fn(node);
+      }
+      for (let i = 0; i < node.children.length; i++) {
+        queue.push(node.children[i]);
+      }
+    }
+  }
+
+  print() {
+    if (!this.root) {
+      return console.log('No root node found');
+    }
+    const newline = new Node('|');
+    const queue = [this.root, newline];
+    let string = '';
+    while (queue.length) {
+      const node = queue.shift();
+      string = `${string}${node.data.toString()}  `;
+      if (node === newline && queue.length) {
+        queue.push(newline);
+      }
+      for (let i = 0; i < node.children.length; i++) {
+        queue.push(node.children[i]);
+      }
+    }
+    console.log(string.slice(0, -2).trim());
+  }
+
+  printByLevel() {
+    if (!this.root) {
+      return console.log('No root node found');
+    }
+    const newline = new Node('\n');
+    const queue = [this.root, newline];
+    let string = '';
+    while (queue.length) {
+      const node = queue.shift();
+      string = `${string}${node.data.toString()}  ${node.data !== '\n' ? ' ' : ''}`;
+      if (node === newline && queue.length) {
+        queue.push(newline);
+      }
+      for (let i = 0; i < node.children.length; i++) {
+        queue.push(node.children[i]);
+      }
+    }
+    console.log(string.trim());
+  }
+
 }
 
 
-var tree = new Tree('CEO');
+const tree = new Tree('CEO');
 
-tree.add('VP of Happiness', 'CEO', tree.traverseBF);
-tree.add('VP of Finance', 'CEO', tree.traverseBF);
-tree.add('VP of Sadness', 'CEO', tree.traverseBF);
-
-tree.add('Director of Puppies', 'VP of Finance', tree.traverseBF);
-tree.add('Manager of Puppies', 'Director of Puppies', tree.traverseBF);
-
-console.log('tree', tree);
+tree.add('ceo');
+tree.add('cto', 'ceo');
+tree.add('dev1', 'cto');
+tree.add('dev2', 'cto');
+tree.add('dev3', 'cto');
+tree.add('cfo', 'ceo');
+tree.add('accountant', 'cfo');
+tree.add('cmo', 'ceo');
+tree.print(); // => ceo | cto cfo cmo | dev1 dev2 dev3 accountant
+tree.printByLevel();  // => ceo \n cto cfo cmo \n dev1 dev2 dev3 accountant
+console.log('tree contains dev1 is true:', tree.contains('dev1')); // => true
+console.log('tree contains dev4 is false:', tree.contains('dev4')); // => false
+console.log('--- BFS');
+tree.traverseBFS(function (node) { console.log(node.data); }); // => ceo cto cfo cmo dev1 dev2 dev3 accountant
+console.log('--- DFS preOrder');
+tree.traverseDFS(function (node) { console.log(node.data); }, 'preOrder'); // => ceo cto dev1 dev2 dev3 cfo accountant cmo
+console.log('--- DFS postOrder');
+tree.traverseDFS(function (node) { console.log(node.data); }, 'postOrder'); // => dev1 dev2 dev3 cto accountant cfo cmo ceo
+tree.remove('cmo');
+tree.print(); // => ceo | cto cfo | dev1 dev2 dev3 accountant
+tree.remove('cfo');
+tree.print(); // => ceo | cto | dev1 dev2 dev3
