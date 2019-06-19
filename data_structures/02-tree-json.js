b75beb2bd7c2414bee2c2f1d9f31f07f9e87bb2e
LUXOFT\IPokromkina <ipokromkina@luxoft.com>
2019-06-19 18:01:53 +0300

    add search for tree

---
+++ data_structures/02-tree-json.js
@@ -0,0 +1,43 @@
+// my realisation json tree data;
+
+
+class Node {
+  constructor(data = {}) {
+    this.id = data.id || '000'; // todo: по хорошему, если нет id надо юзать uid библиотеку для генерации
+    this.parent = data.parent || null;
+    this.title = data.title || '';
+    this.text = data.text || '';
+    this.children = [];
+  }
+}
+
+class Tree {
+  constructor(data = {}) {
+    this.root = [new Node(data)];
+  }
+
+  add(nodeData, parent) {
+    console.log('add nodeData, parent', {nodeData, parent});
+  }
+
+  deepSearch(nodeId, node = this.root) {
+    console.log('deepSearch');
+    // if(nodeId) {
+      node.forEach(node => {
+        if (node.id === nodeId) {
+          console.log('we get it', node);
+          return node;
+        } else {
+          console.log('search in children', node.children);
+          if (node.children.length !== 0) { 
+            this.deepSearch(node.children, nodeId);
+          }
+        }
+      });
+    // }
+  }
+
+}
+
+const tree = new Tree();
+tree.deepSearch("000");
